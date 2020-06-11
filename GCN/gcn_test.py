2893cb5abaa0fbc943b8a84dc7f12989549d72a1
chenfahao <chenfh612@163.com>
2020-06-11 21:55:26 +0900

    Parallel computing GCN

--- GCN/gcn_test.py
+++ GCN/gcn_test.py
@@ -11,21 +11,27 @@ from dgl.data import register_data_args, load_data
 from dgl.data import citation_graph as citegrh
 from dgl.data import RedditDataset
 import pandas as pd
+import networkx as nx
 #import thread
 import threading
+import multiprocessing as mp
+import psutil
+import os
 
 acc=0
 
 class myThread (threading.Thread):
-    def __init__(self, threadID, name):
+    def __init__(self, threadID, name,num_neighbors,flag):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.num_neighbors = num_neighbors
+        self.flag = flag
         #self.counter = counter
     def run(self):
         print ('Starting ' + self.name)
         threadLock.acquire()
-        start(self.name,2)
+        start(self.name,self.num_neighbors,self.flag)
         threadLock.release()
 
 threadLock = threading.Lock()
@@ -128,147 +134,155 @@ class GCNInfer(nn.Module):
         h = nf.layers[-1].data.pop('activation')
         return h
 
-
-def main(thread_name,args):
-    # load and preprocess dataset
-    # data = load_data(args)
-
-    out = [0]
-    data = citegrh.load_pubmed()
-    # data = RedditDataset(self_loop=True)
-    if args.self_loop and not args.dataset.startswith('reddit'):
-        data.graph.add_edges_from([(i,i) for i in range(len(data.graph))])
-
-    train_nid = np.nonzero(data.train_mask)[0].astype(np.int64)
-    test_nid = np.nonzero(data.test_mask)[0].astype(np.int64)
+# create the subgraph
+def load_cora_data(list1, list2):
+    
+    # data = citegrh.load_pubmed()
+    data = RedditDataset(self_loop=True)
 
     features = torch.FloatTensor(data.features)
     labels = torch.LongTensor(data.labels)
-    if hasattr(torch, 'BoolTensor'):
-        train_mask = torch.BoolTensor(data.train_mask)
-        val_mask = torch.BoolTensor(data.val_mask)
-        test_mask = torch.BoolTensor(data.test_mask)
-    else:
-        train_mask = torch.ByteTensor(data.train_mask)
-        val_mask = torch.ByteTensor(data.val_mask)
-        test_mask = torch.ByteTensor(data.test_mask)
-    in_feats = features.shape[1]
-    n_classes = data.num_labels
-    n_edges = data.graph.number_of_edges()
-
-    n_train_samples = train_mask.int().sum().item()
-    n_val_samples = val_mask.int().sum().item()
-    n_test_samples = test_mask.int().sum().item()
-
-    print("""----Data statistics------'
-      #Edges %d
-      #Classes %d
-      #Train samples %d
-      #Val samples %d
-      #Test samples %d""" %
-          (n_edges, n_classes,
-              n_train_samples,
-              n_val_samples,
-              n_test_samples))
-
-    # create GCN model
+    train_mask = torch.BoolTensor(data.train_mask)
+    test_mask = torch.BoolTensor(data.test_mask)
+    train_nid = np.nonzero(data.train_mask)[0].astype(np.int64)
+    test_nid = np.nonzero(data.test_mask)[0].astype(np.int64)
+    g = data.graph
+    # add self loop
+    #g.remove_edges_from(nx.selfloop_edges(g))
     g = DGLGraph(data.graph, readonly=True)
+    n_classes = data.num_labels
+    # g.add_edges(g.nodes(), g.nodes())
+
     norm = 1. / g.in_degrees().float().unsqueeze(1)
-
-    if args.gpu < 0:
-        cuda = False
-    else:
-        cuda = True
-        torch.cuda.set_device(args.gpu)
-        features = features.cuda()
-        labels = labels.cuda()
-        train_mask = train_mask.cuda()
-        val_mask = val_mask.cuda()
-        test_mask = test_mask.cuda()
-        norm = norm.cuda()
-
     g.ndata['features'] = features
-
-    num_neighbors = args.num_neighbors
-
+    # num_neighbors = args.num_neighbors
     g.ndata['norm'] = norm
 
-    model = GCNSampling(in_feats,
+    in_feats = features.shape[1]
+    n_test_samples = test_mask.int().sum().item()
+
+    # src, dst = g.all_edges()
+    # src = src.detach().numpy()
+    # dst = dst.detach().numpy()
+    # edge_list = list(zip(src, dst))  
+
+    g1 = g.subgraph(list1)  
+    g1.copy_from_parent()  
+    g1.readonly()
+    g2 = g.subgraph(list2)  
+    g2.copy_from_parent()  
+    g2.readonly()
+    #g.readonly()
+
+    labels1 = labels[list1]
+    labels2 = labels[list2]
+
+    train_nid1 = []
+    train_nid2 = []
+    for i in range(len(list1)):
+        if list1[i] in train_nid:
+            train_nid1.append(i)
+    train_nid1 = np.array(train_nid1)
+
+    for i in range(len(list2)):
+        if list2[i] in train_nid:
+            train_nid2.append(i)
+    train_nid2 = np.array(train_nid2)
+
+    return g, g1, g2, labels, labels1, labels2, train_nid, train_nid1, train_nid2, test_nid, in_feats, n_classes, n_test_samples
+
+# run a subgraph
+def runGraph(Model,Graph,args,Optimizer,Labels,Train_nid,pipe):
+    loss_fcn = nn.CrossEntropyLoss()
+    for epoch in range(args.n_epochs):
+        for nf in dgl.contrib.sampling.NeighborSampler(Graph, args.batch_size,  
+                                                            args.num_neighbors,
+                                                            neighbor_type='in',
+                                                            shuffle=True,
+                                                            num_workers=32,
+                                                            num_hops=args.n_layers+1,
+                                                            seed_nodes=Train_nid):
+            nf.copy_from_parent()
+            Model.train()
+            # forward
+            pred = Model(nf)
+            batch_nids = nf.layer_parent_nid(-1).to(device=pred.device, dtype=torch.long)  
+            batch_labels = Labels[batch_nids]
+            loss = loss_fcn(pred, batch_labels)
+            Optimizer.zero_grad()
+            loss.backward()
+            Optimizer.step()
+        p = Model.state_dict()
+        pipe.send(p)
+        p1 = pipe.recv()
+        Model.load_state_dict(p1)
+        pipe.send(Model)
+        print(os.getpid()," in round ",epoch,' compeleted')
+
+
+
+# generate the subgraph's model and optimizer
+def genGraph(args,Graph,Labels,Train_nid,In_feats,N_classes,N_test_samples):
+    model = GCNSampling(In_feats,
                         args.n_hidden,
-                        n_classes,
+                        N_classes,
                         args.n_layers,
                         F.relu,
                         args.dropout)
 
-    if cuda:
-        model.cuda()
-
-    loss_fcn = nn.CrossEntropyLoss()
-
-    infer_model = GCNInfer(in_feats,
-                           args.n_hidden,
-                           n_classes,
-                           args.n_layers,
-                           F.relu)
-
-    if cuda:
-        infer_model.cuda()
-
-    # use optimizer
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=args.lr,
                                  weight_decay=args.weight_decay)
+    
+    return model, optimizer
 
-    # initialize graph
-    dur = []
+def inference(Graph,args,Labels,Test_nid,Train_nid,Train_nid1,Train_nid2,In_feats,N_classes,N_test_samples,pipe_1,pipe_2):
+
     for epoch in range(args.n_epochs):
-        for nf in dgl.contrib.sampling.NeighborSampler(g, args.batch_size,
-                                                       args.num_neighbors,
-                                                       neighbor_type='in',
-                                                       shuffle=True,
-                                                       num_workers=32,
-                                                       num_hops=args.n_layers+1,
-                                                       seed_nodes=train_nid):
-            nf.copy_from_parent()
-            model.train()
-            # forward
-            pred = model(nf)
-            batch_nids = nf.layer_parent_nid(-1).to(device=pred.device, dtype=torch.long)
-            batch_labels = labels[batch_nids]
-            loss = loss_fcn(pred, batch_labels)
+        p1 = pipe_1.recv()
+        p2 = pipe_2.recv()
+        
+        infer_model = GCNInfer(In_feats,
+                            args.n_hidden,
+                            N_classes,
+                            args.n_layers,
+                            F.relu)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        for key, value in p2.items():  
+                p1[key] = p1[key] * (len(Train_nid1) / len(Train_nid)) + p2[key] * (len(Train_nid2) / len(Train_nid))
 
-        for infer_param, param in zip(infer_model.parameters(), model.parameters()):
+            # model1.load_state_dict(p1)  
+            # model2.load_state_dict(p1)  
+        pipe_1.send(p1)
+        pipe_2.send(p1)
+        model1 = pipe_1.recv()
+        model2 = pipe_2.recv()
+        for infer_param, param in zip(infer_model.parameters(), model1.parameters()):  
             infer_param.data.copy_(param.data)
-        # for name in model.state_dict():
-        #     print(name)
+
         num_acc = 0.
 
-        for nf in dgl.contrib.sampling.NeighborSampler(g, args.test_batch_size,
-                                                       g.number_of_nodes(),
-                                                       neighbor_type='in',
-                                                       num_workers=32,
-                                                       num_hops=args.n_layers+1,
-                                                       seed_nodes=test_nid):
+        for nf in dgl.contrib.sampling.NeighborSampler(Graph, args.test_batch_size,
+                                                        g.number_of_nodes(),
+                                                        neighbor_type='in',
+                                                        num_workers=32,
+                                                        num_hops=args.n_layers+1,
+                                                        seed_nodes=Test_nid):
             nf.copy_from_parent()
             infer_model.eval()
             with torch.no_grad():
                 pred = infer_model(nf)
                 batch_nids = nf.layer_parent_nid(-1).to(device=pred.device, dtype=torch.long)
-                batch_labels = labels[batch_nids]
+                batch_labels = Labels[batch_nids]
                 num_acc += (pred.argmax(dim=1) == batch_labels).sum().cpu().item()
-        
         acc = round(num_acc/n_test_samples,4)
-        out.append(acc)
-        print(thread_name," round: ",epoch," Test Accuracy :", acc)
-    dataframe = pd.DataFrame({'acc':out})
-    dataframe.to_csv("non-dqn-4.csv",header = False,index=False,sep=',')
+        print("round: ",epoch," Test Accuracy :", acc)
+
+    # dataframe = pd.DataFrame({'acc':out})
+    # dataframe.to_csv("non-dqn-4.csv",header = False,index=False,sep=',')
 
 
-def start(thread_name,num_neighbors):
+def Gen_args(num_neighbors):
     parser = argparse.ArgumentParser(description='GCN')
     register_data_args(parser)
     parser.add_argument("--dropout", type=float, default=0.5,
@@ -296,53 +310,37 @@ def start(thread_name,num_neighbors):
     parser.add_argument("--weight-decay", type=float, default=5e-4,
             help="Weight for L2 loss")
     args = parser.parse_args()
-    main(thread_name,args)
+    return args
 
 
 if __name__ == '__main__':
-    thread1 = myThread(1, 'Thread-1')
-    thread2 = myThread(2, 'Thread-2')
+    args = Gen_args(5)   # return the parameters
 
-    thread1.start()
-    thread2.start()
+    # communication
+    pipe1 = mp.Pipe()
+    pipe2 = mp.Pipe()
 
-    threads.append(thread1)
-    threads.append(thread2)
-    for t in threads:
-        t.join()
-    print ('Exiting Main Thread')
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='GCN')
-#     register_data_args(parser)
-#     parser.add_argument("--dropout", type=float, default=0.5,
-#             help="dropout probability")
-#     parser.add_argument("--gpu", type=int, default=-1,
-#             help="gpu")
-#     parser.add_argument("--lr", type=float, default=0.01,
-#             help="learning rate")
-#     parser.add_argument("--n-epochs", type=int, default=200,
-#             help="number of training epochs")
-#     parser.add_argument("--batch-size", type=int, default=1000,
-#             help="batch size")
-#     parser.add_argument("--test-batch-size", type=int, default=1000,
-#             help="test batch size")
+    out = [0]
 
-#     parser.add_argument("--num-neighbors", type=int, default=3,
-#             help="number of neighbors to be sampled")
+    node_list = list(range(232965))  
+    list1 = node_list[0::2]
+    list2 = list(set(node_list) - set(list1))
+    g, g1, g2, labels, labels1, labels2, train_nid, train_nid1, train_nid2, test_nid, in_feats, n_classes, n_test_samples = load_cora_data(list1, list2)
+    
+    model1, optimizer1 = genGraph(args,g1,labels1,train_nid1,in_feats,n_classes,n_test_samples)
+    model2, optimizer2 = genGraph(args,g2,labels2,train_nid2,in_feats,n_classes,n_test_samples)
 
-#     parser.add_argument("--n-hidden", type=int, default=16,
-#             help="number of hidden gcn units")
-#     parser.add_argument("--n-layers", type=int, default=1,
-#             help="number of hidden gcn layers")
-#     parser.add_argument("--self-loop", action='store_true',
-#             help="graph self-loop (default=False)")
-#     parser.add_argument("--weight-decay", type=float, default=5e-4,
-#             help="Weight for L2 loss")
-#     args = parser.parse_args()
+    task1 = mp.Process(target=runGraph,args=(model1,g1,args,optimizer1,labels1,train_nid1,pipe1[0],))
+    task2 = mp.Process(target=runGraph,args=(model2,g2,args,optimizer2,labels2,train_nid1,pipe2[0],))
+    task3 = mp.Process(target=inference,args=(g,args,labels,test_nid,train_nid,train_nid1,train_nid2,in_feats,n_classes,n_test_samples,pipe1[1],pipe2[1],))
 
-#     print(args)
+    task1.start()
+    task2.start()
+    task3.start()
 
-#     main(args)
+    task1.join()
+    task2.join()
+    task3.join()
 
 
